3dccb84075703290abf765d69471dce5c2180190
Crisoforo <zs19023584@estudiantes.uv.mx>
2023-11-16 22:52:47 -0600

    Add project files.

---
+++ webapifoto/Controllers/FotoController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using webapifoto.Models;
+
+namespace webapifoto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FotoController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FotoController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Foto>> PostIndex([FromForm] Foto foto)
+        {
+            try
+            {
+                string webRootPath = _webHostEnvironment.WebRootPath;
+                string rutaArchivos = Path.Combine(webRootPath, "files");
+
+                if(foto.Archivo.Length > 0)
+                {
+                    if(!Directory.Exists(rutaArchivos))
+                    {
+                        Directory.CreateDirectory(rutaArchivos);
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(Path.Combine(rutaArchivos, foto.Archivo.FileName)))
+                    {
+                        await foto.Archivo.CopyToAsync(fileStream);
+                        fileStream.Flush();
+                    }
+                    foto.Url = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/files/" + foto.Archivo.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message + _webHostEnvironment.WebRootPath);
+            }
+            return CreatedAtAction(nameof(PostIndex), new { foto.Nombre, foto.Url });
+        }
+    }
+}
